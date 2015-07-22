eb4683ade410611c58151c24459bbf571b27c415
Andrew Buntine Of Doom <bunts@hhd.com.au>
2015-07-22 21:30:00 +1000

    Initial guess

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("!!! GUESS THE RANDOM NUMBER (1 - 100) !!!");
+
+    println!("Please input yuor number: ");
+
+    let mut guess = String::new();
+
+    io::stdin().read_line(&mut guess)
+        .ok()
+        .expect("Failed to read line");
+
+    println!("You guessed: {}", guess);
 }

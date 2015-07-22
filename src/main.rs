5ae5dd1e039517736fe1432f8b547f8e68f9095b
Andrew Buntine Of Doom <bunts@hhd.com.au>
2015-07-22 22:57:53 +1000

    Added looping

--- src/main.rs
+++ src/main.rs
@@ -11,23 +11,28 @@ fn main() {
 
     println!("The secret number is: {}", secret_number);
 
-    println!("Please input your number: ");
+    loop {
+        println!("Please input your number: ");
 
-    let mut guess = String::new();
+        let mut guess = String::new();
 
-    io::stdin().read_line(&mut guess)
-        .ok()
-        .expect("Failed to read line");
+        io::stdin().read_line(&mut guess)
+            .ok()
+            .expect("Failed to read line");
 
-    let guess: u32 = guess.trim().parse()
-        .ok()
-        .expect("Please number a number!");
+        let guess: u32 = guess.trim().parse()
+            .ok()
+            .expect("Please number a number!");
 
-    println!("You guessed: {}", guess);
+        println!("You guessed: {}", guess);
 
-    match guess.cmp(&secret_number) {
-        Ordering::Less    => println!("Too small!"),
-        Ordering::Equal   => println!("You win!"),
-        Ordering::Greater => println!("Too large!"),
+        match guess.cmp(&secret_number) {
+            Ordering::Less    => println!("Too small!"),
+            Ordering::Greater => println!("Too large!"),
+            Ordering::Equal   => {
+                println!("You win!");
+                break;
+            },
+        }
     }
 }
